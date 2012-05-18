567a75e6e8e56566d15319d7a2a739fa712524e8
Joshua Carver <jcarver989@gmail.com>
2012-05-18 10:21:43 -0700

    updated docs to look a tad more bizo

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
 TAG=`git describe | cut -d '-' -f1`
 BIZSTRAP=bizstrap-$TAG.css
-RELEASE_PATH=s3://com-bizo-public/bizstrap/$BIZSTRAP
+BASE_PATH=s3://com-bizo-public/bizstrap
+CSS_RELEASE_PATH=$BASE_PATH/css/$BIZSTRAP
+IMG_RELEASE_PATH=$BASE_PATH/img
+JS_RELEASE_PATH=$BASE_PATH/js
 
 ./build.sh $BIZSTRAP 
 
-echo "Uploading to s3..."
-s3cp $BIZSTRAP $RELEASE_PATH 
-s3mod $RELEASE_PATH public-read
+echo "Uploading CSS to s3..."
+s3cp $BIZSTRAP $CSS_RELEASE_PATH 
+s3mod $CSS_RELEASE_PATH public-read
+
+echo "Uploading images to s3..."
+s3cp img $BASE_PATH -r
+s3mod $IMG_RELEASE_PATH/logo.png public-read
+s3mod $IMG_RELEASE_PATH/glyphicons-halflings-white.png public-read
+s3mod $IMG_RELEASE_PATH/glyphicons-halflings.png public-read
 
 echo "Cleaning Up..."
 rm $BIZSTRAP
 
-echo "Done: Bizstrap is available at: http://media.bizo.com/bizstrap/${BIZSTRAP}"
+echo "Done: Bizstrap is available at: http://media.bizo.com/bizstrap/css/${BIZSTRAP}"
