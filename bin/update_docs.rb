542ad599f36be681be97165f2fbff793ee661e77
Joshua Carver <jcarver989@gmail.com>
2012-12-12 16:26:55 -0800

    switch print statement

--- bin/update_docs.rb
+++ bin/update_docs.rb
@@ -6,7 +6,7 @@ file_location = File.join("jekyll_docs", "pages", "index.html")
 text = File.read(file_location)
 text.gsub!(/http:\/\/media.bizo.com\/bizstrap\/css\/bizstrap-v([^\s]+).css/, "http://media.bizo.com/bizstrap/css/bizstrap-#{latest_tag}.css")
 
-puts "Updated docs to: #{latest_tag}"
 File.open(file_location, 'w') { |f| f.write(text) }
+puts "Updated docs to: #{latest_tag}"
 
 
